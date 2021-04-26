db16464850acafc1fd9c03ebb4a14a4bbee432df
cofoeducistudent <cofoedu91@gmail.com>
2021-04-27 00:41:46 +0100

    core refactor

--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,14 @@
 
-
- 
-
 onload = function () {
 
     this.setInterval(collapseIt, 6000);
 
-
 };
 
-
 function collapseIt() {
-
- 
 
     let element = document.getElementById('navbarNavAltMarkup');
 
     element.classList.remove("show");
-
 
 }
